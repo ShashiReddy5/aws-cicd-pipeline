824cf313d43b361d70546518f184aa26762079d9
Shashidhar Reddy <shashirdy74@gmail.com>
2026-07-09 10:28:20 -0400

    Create PipelineDemoApplicationTests.java

---
+++ src/test/java/com/shashi/pipeline/PipelineDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.shashi.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
